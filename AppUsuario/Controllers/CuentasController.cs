0f207ebcf3ac053554792f6670ac39712efdf372
QiangRO <79667563+QiangRO@users.noreply.github.com>
2022-11-11 10:36:45 -0400

    Ultimos cambios

--- AppUsuario/Controllers/CuentasController.cs
+++ AppUsuario/Controllers/CuentasController.cs
@@ -1,11 +1,86 @@
 ﻿using AppUsuario.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppUsuario.Controllers
 {
     public class CuentasController : Controller
     {
+        //Inyeccion de dependencias
+        private readonly UserManager<IdentityUser>? _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public CuentasController(UserManager<IdentityUser>? userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Registro(RegistroVM registroVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var usuario = new UserModel
+                {
+                    UserName = registroVM.Email,
+                    Email = registroVM.Email,
+                    Nombre = registroVM.Nombre,
+                    Url= registroVM.Url,
+                    CodigoPais = registroVM.CodigoPais,
+                    Telefono = registroVM.Telefono,
+                    Pais = registroVM.Pais,
+                    Ciudad = registroVM.Ciudad,
+                    Direccion = registroVM.Direccion,
+                    FechaNacimiento = registroVM.FechaNacimiento,
+                    Estado = registroVM.Estado
+
+                };
+                var resultado = await _userManager.CreateAsync(usuario, registroVM.Password);
+                if (resultado.Succeeded)
+                {
+                    await _signInManager.SignInAsync(usuario, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                ValidarErrores(resultado);
+            }
+
+            return View(registroVM);
+        }
+        private void ValidarErrores(IdentityResult resultado)
+        {
+            foreach(var error in resultado.Errors)
+            {
+                ModelState.AddModelError(String.Empty, error.Description);
+            }
+        }
+        [HttpGet]
+        public IActionResult Acceso()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>Acceso(AccesoVM accesoVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var resultado = await _signInManager.PasswordSignInAsync(accesoVM.Email,
+                    accesoVM.Password,
+                    accesoVM.RememberMe,
+                    lockoutOnFailure: false);
+                if (resultado.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(String.Empty, "Acceso invalido");
+                    return View(accesoVM);
+                }
+            }
+            return View(accesoVM);
+        }
         // GET: CuentasController
         public ActionResult Index()
         {
@@ -17,73 +92,12 @@ namespace AppUsuario.Controllers
             RegistroVM registroVM = new RegistroVM();
             return View(registroVM);
         }
-        // GET: CuentasController/Details/5
-        public ActionResult Details(int id)
+        public async Task<IActionResult> SalirAplicacion()
         {
-            return View();
-        }
-
-        // GET: CuentasController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: CuentasController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: CuentasController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: CuentasController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: CuentasController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: CuentasController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            //SALE DE LA APLICACION Y DESTRUYE LAS COOKIES
+            await _signInManager.SignOutAsync();
+            //REDIRECCIONA A LA PAGINA DE INICIO
+            return RedirectToAction(nameof(HomeController.Index), "Home");
         }
     }
 }
